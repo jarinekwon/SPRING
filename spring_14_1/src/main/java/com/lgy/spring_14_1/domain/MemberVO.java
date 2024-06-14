04b2b55da5396bb94e095e8dcddfabc1b3399b77
kgamgak@gmail.com <kgamgak@gmail.com>
2024-06-14 17:05:17 +0900

    all files

---
+++ spring_14_1/src/main/java/com/lgy/spring_14_1/domain/MemberVO.java
@@ -0,0 +1,12 @@
+package com.lgy.spring_14_1.domain;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+
+	private String id;
+	private String pw;
+	private String name;
+	private String email;
+}
